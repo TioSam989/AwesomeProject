9253ae23d3720ce21e3cb877b7f285b6f256a955
tiosam989 <davi.neves.santos2@gmail.com>
2023-11-16 11:47:32 +0000

    updating app

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+
+  function bookInputHandler(enteredText) {
+
+  }
+   function addBookHandler(){
+
+   }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Add ur book' onChangeText={bookInputHandler} />
+        <Button title="Add book" onPress={addBookHandler} />
+      </View>
+      
+      <View style={styles.booksContainer}>
+        <Text>List of books</Text>
+      </View>
+
     </View>
   );
-}
+f}
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8
+  },
+  booksContainer: {
+
+    flex: 4
+  }
+
 });
