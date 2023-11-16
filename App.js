9988750e2b9b41ed346275e5027b096032e96c5c
tiosam989 <davi.neves.santos2@gmail.com>
2023-11-16 12:50:31 +0000

    using state as well

--- App.js
+++ App.js
@@ -3,28 +3,37 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
 
+  const [enteredBookText, setEnteredBookText] = useState('') //the text inserted in the input and eh action of SET IN 
+  const [books, setBooks] = useState([]) //is an array cuz is a list of books but has no books yet
+
   function bookInputHandler(enteredText) {
-
+    setEnteredBookText(enteredText)
   }
-   function addBookHandler(){
+  function addBookHandler() {
+    // setBooks([...books, enteredBookText]) // works but not the better way to update an state
 
-   }
+    setBooks((currentBooks) => [
+      ...books,
+      enteredBookText,
+    ]); // recommended way, with aero func
+  }
 
   return (
     <View style={styles.appContainer}>
-      
+
       <View style={styles.inputContainer}>
         <TextInput style={styles.textInput} placeholder='Add ur book' onChangeText={bookInputHandler} />
         <Button title="Add book" onPress={addBookHandler} />
       </View>
-      
+
       <View style={styles.booksContainer}>
-        <Text>List of books</Text>
+        {books.map((book) => <Text key={book}>{book}</Text>)}
       </View>
 
     </View>
   );
-f}
+  f
+}
 
 const styles = StyleSheet.create({
   appContainer: {
